fc797881b488c6744b57c4fee203f76dbb052be1
brin2806 <brin2806@gmail.com>
2015-02-25 20:51:52 +1100

    uploading test file

---
+++ Classes_test.py
@@ -0,0 +1,11 @@
+# this is the test file to import and test the String Class that was created
+
+from Classes_String import *
+
+if __name__ == "__main__":
+
+    string_object = String()
+    string_object.get_string()
+    string_object.print_string()
+
+# Hi Victor why does my module run twice before finishing?
